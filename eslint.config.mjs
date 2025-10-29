9622aaa2c388b16384ba13264fa868e7d27bee48
noeyigg <miyg2003@naver.com>
2025-10-29 16:48:10 +0900

    setting: 화살표함수 사용 규칙 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,26 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
+import preferArrow from "eslint-plugin-prefer-arrow";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+  {
+    plugins: {
+      "prefer-arrow": preferArrow,
+    },
+    rules: {
+      "prefer-arrow/prefer-arrow-functions": [
+        "error",
+        {
+          disallowPrototype: true,
+          singleReturnOnly: false,
+          classPropertiesAllowed: false,
+          allowStandaloneDeclarations: false,
+        },
+      ],
+    },
+  },
 ]);
 
 export default eslintConfig;
