7b83a26be1c2924c2cd76f94ceda16635d0c6d49
SooY2 <ssy67366736@gmail.com>
2025-11-05 11:33:13 +0900

    refactor: metadata에서 viewport 분리

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,13 @@ import AppLayout from "@/components/layouts/AppLayout";
 export const metadata = {
   title: "WORKET",
   description: "긱노동자 간편한 소득증빙 플랫폼",
-  viewport:
-    "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: "no",
 };
 
 const RootLayout = ({ children }) => {
