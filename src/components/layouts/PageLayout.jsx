cb51f6ed2d9e0597edfc885dbe8e53a201e5d8c5
SooY2 <ssy67366736@gmail.com>
2025-11-05 14:59:03 +0900

    feat: 각 페이지 layout.js 에서 사용할 PageLayout.jsx 구현

---
+++ src/components/layouts/PageLayout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+/**
+ * 페이지 공통 레이아웃
+ * - header가 존재하면 상단 여백(pt-[6rem]) 자동 추가
+ * - 기본적으로 세로 전체 차지
+ */
+const PageLayout = ({ children, header = null }) => {
+  return (
+    <div className="flex flex-col h-full">
+      {header}
+      <div className={`${header ? "pt-[6rem]" : ""} flex-1 h-full`}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default PageLayout;
