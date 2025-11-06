73424cdcb53fd724f6fc82c1453d9bb1fe77c22d
SooY2 <ssy67366736@gmail.com>
2025-11-06 10:23:38 +0900

    chore: showBack -> canGoBack 변수명 수정

--- src/app/transactions/layout.js
+++ src/app/transactions/layout.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import Header from "@/components/common/Header";
 import PageLayout from "@/components/layouts/PageLayout";
 
@@ -15,28 +15,28 @@ const TransactionLayout = ({ children }) => {
 
   // 헤더 정보 설정
   let headerTitle = "거래 관리";
-  let showBack = false;
+  let canGoBack = false;
 
   if (pathname === "/transactions") {
     headerTitle = "거래 관리";
-    showBack = false; // 루트 페이지라 뒤로가기 없음
+    canGoBack = false; // 루트 페이지라 뒤로가기 없음
   } else if (pathname === "/transactions/create") {
     headerTitle = "거래 등록";
-    showBack = true;
+    canGoBack = true;
   } else if (pathname === "/transactions/upload") {
     headerTitle = "거래 업로드";
-    showBack = true;
+    canGoBack = true;
   } else if (/^\/transactions\/\d+$/.test(pathname)) {
     headerTitle = "거래 상세";
-    showBack = true;
+    canGoBack = true;
   } else if (/^\/transactions\/\d+\/create-link$/.test(pathname)) {
     headerTitle = "거래 링크 생성";
-    showBack = true;
+    canGoBack = true;
   }
 
   return (
     <PageLayout
-      header={<Header title={headerTitle} onBack={showBack ? 1 : null} />}
+      header={<Header title={headerTitle} onBack={canGoBack ? 1 : null} />}
     >
       {children}
     </PageLayout>
