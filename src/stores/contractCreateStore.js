a04f7cbba9a9d08f3e9969c6ad0674d416994bd3
SooY2 <ssy67366736@gmail.com>
2025-11-06 16:06:03 +0900

    chore: 반환값 주석 추가

--- src/stores/contractCreateStore.js
+++ src/stores/contractCreateStore.js
@@ -2,12 +2,20 @@ import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
 /**
- * @typedef {Object} ContractUploadData
- * @property {string} title
- * @property {number} amount
- * @property {string} client_name
- * @property {string} client_phone
- * @property {string} freelancer_sign
+ * @typedef {Object} ContractData
+ * @property {string} title 계약 제목
+ * @property {number} amount 계약 금액
+ * @property {string} client_name 클라이언트 이름
+ * @property {string} client_phone 클라이언트 전화번호
+ * @property {string} freelancer_sign 프리랜서 서명 URL
+ */
+
+/**
+ * 반환값
+ * @typedef {Object} ContractCreateStore
+ * @property {ContractData} contract 현재 작성 중인 계약 데이터
+ * @property {(key: keyof ContractData, value: any) => void} setField 개별 필드 업데이트 함수
+ * @property {() => void} reset 계약 데이터를 초기 상태로 리셋하는 함수
  */
 
 export const useContractCreateStore = create(
