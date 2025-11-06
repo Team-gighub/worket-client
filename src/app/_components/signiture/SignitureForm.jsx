2557677e9a40f8701defe65c039caf8af0f53ccb
yuunseo <dbstj7457@naver.com>
2025-11-06 10:23:27 +0900

    refactor: 바텀시트 관련 함수를 분리하여 별도의 훅으로 변경

--- src/app/_components/signiture/SignitureForm.jsx
+++ src/app/_components/signiture/SignitureForm.jsx
@@ -2,14 +2,14 @@
 import React from "react";
 import BottomSheet from "../BottomSheet";
 import Signiture from "./Signiture";
-import useSigniture from "./useSigniture";
+import useBottomSheet from "../useBottomSheet";
 
-const SignatureForm = ({ onSave }) => {
-  const { isOpen, openSheet, closeSheet, handleSave } = useSigniture(onSave);
+const SignatureForm = () => {
+  const { isOpen, open, close } = useBottomSheet();
 
   return (
     <div className="relative w-full h-full">
-      <button onClick={openSheet} className="w-full h-[19rem] p-[2rem]">
+      <button onClick={open} className="w-full h-[19rem] p-[2rem]">
         <div className="bg-basic-200 w-full h-full relative rounded-lg">
           <span className="absolute top-4 left-5 pretendard-semibold-16 text-basic-400">
             서명란
@@ -17,8 +17,8 @@ const SignatureForm = ({ onSave }) => {
         </div>
       </button>
 
-      <BottomSheet isOpen={isOpen} onClose={closeSheet}>
-        <Signiture onSave={handleSave} />
+      <BottomSheet isOpen={isOpen} onClose={close}>
+        <Signiture />
       </BottomSheet>
     </div>
   );
