1f29ca0a2b47a79c204c65743f65b7235a1fd7c6
SooY2 <ssy67366736@gmail.com>
2025-11-07 10:57:49 +0900

    feat: trade layout 생성

---
+++ src/app/trade/[id]/layout.js
@@ -0,0 +1,7 @@
+import PageLayout from "@/components/layouts/PageLayout";
+
+const tradeLayout = ({ children }) => {
+  return <PageLayout>{children}</PageLayout>;
+};
+
+export default tradeLayout;
