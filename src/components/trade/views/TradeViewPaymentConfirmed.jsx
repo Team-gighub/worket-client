3ed2b72f6a31b66f0c0e8fd4386e3cecb7d476d7
SooY2 <ssy67366736@gmail.com>
2025-11-10 19:17:11 +0900

    feat: status-PAYMENT_CONFIRMED view

--- src/components/trade/views/TradeViewPaymentConfirmed.jsx
+++ src/components/trade/views/TradeViewPaymentConfirmed.jsx
@@ -1,12 +1,18 @@
 import TradeViewLayout from "@/components/layouts/TradeViewLayout";
 import TradeStepIndicator from "../TradeStepIndicator";
 import InfoText from "@/components/common/InfoText";
+import { useTradeDataStore } from "@/stores/tradeDataStore";
 
 const TradeViewPaymentConfirmed = () => {
+  const { tradeData } = useTradeDataStore();
+  const { clientName, freelancerName } = tradeData;
   return (
     <TradeViewLayout>
       <InfoText
-        mainTexts={["000(갑)님과 000(을)님의", "거래 페이지입니다."]}
+        mainTexts={[
+          `${clientName}님과 ${freelancerName}님의`,
+          "거래 페이지입니다.",
+        ]}
         subText="지급 확정을 누르셨어요! 빠른 시일 내에 대금이 전달될 거예요."
       />
       <TradeStepIndicator currentStep={4} />
