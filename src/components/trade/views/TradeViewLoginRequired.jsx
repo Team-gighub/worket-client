2b800ef8e5463c36b5b43ae998cfaf58f973a80f
SooY2 <ssy67366736@gmail.com>
2025-11-10 19:15:03 +0900

    feat: 로그인 전 뷰

--- src/components/trade/views/TradeViewLoginRequired.jsx
+++ src/components/trade/views/TradeViewLoginRequired.jsx
@@ -1,5 +1,28 @@
-const TradeViewLoginRequired = () => {
-  return <div>로그인 전 화면</div>;
+"use client";
+
+import InfoText from "@/components/common/InfoText";
+import TradeViewLayout from "@/components/layouts/TradeViewLayout";
+import TradeStepIndicator from "../TradeStepIndicator";
+import MainButton from "@/components/common/MainButton";
+
+const TradeViewLoginRequired = ({ clientName, freelancerName }) => {
+  return (
+    <TradeViewLayout>
+      <InfoText
+        mainTexts={[
+          `${clientName}님과 ${freelancerName}님의`,
+          "거래 페이지입니다.",
+        ]}
+        subText="거래를 시작하기 전, 본인인증을 진행해주세요"
+      />
+      <TradeStepIndicator currentStep={0} />
+      {/* TODO: 카카오 로그인 버튼 or 로크인 버튼으로 수정 & 클릭 액션 추가*/}
+      <MainButton
+        text="본인인증 하러가기"
+        onClick={() => console.log("로그인 화면으로 이동")}
+      />
+    </TradeViewLayout>
+  );
 };
 
 export default TradeViewLoginRequired;
