e88420ad1b4cf794260773f16289c06d07803fa4
yuunseo <dbstj7457@naver.com>
2025-11-10 10:51:45 +0900

    feat: 계약서 미리보기 zustand 적용

--- src/app/transactions/create/result/page.jsx
+++ src/app/transactions/create/result/page.jsx
@@ -1,27 +1,44 @@
+"use client";
 import "@/app/globals.css";
 import { React } from "react";
 import InfoText from "@/components/common/InfoText";
 import InfoCard from "@/components/common/InfoCard";
 import SignatureForm from "@/components/common/SignatureForm";
 import DualButtons from "@/components/common/DualButtons";
+import { useContractCreateStore } from "@/stores/contractCreateStore";
 
 const CreateResultPage = () => {
-  /* TODO: 더미데이터 추후 수정 필요 */
+  /* 데이터 포맷팅을 위한 함수 */
+  const formatPhone = (phone) => {
+    if (!phone) return "";
+    return phone.replace(/(\d{3})(\d{3,4})(\d{4})/, "$1-$2-$3");
+  };
+  const formatAmount = (amount) => {
+    if (!amount) return "";
+    return Number(amount).toLocaleString(); // 123124 → 123,124
+  };
+
+  /* TODO: 프리랜서 개인정보 추후 수정 필요 */
+  const { contract } = useContractCreateStore();
+
   const contract_data = [
-    { label: "계약명", value: "프로젝트명" },
-    { label: "계약기간", value: "2025.07.01~2025.12.12" },
-    { label: "계약금액", value: "300,000원" },
+    { label: "계약명", value: contract.title || "" },
+    {
+      label: "계약기간",
+      value: `${contract.start_date} ~ ${contract.end_date}`,
+    },
+    { label: "계약금액", value: `${formatAmount(contract.amount)}원` },
   ];
 
   const contractor_data = [
-    { label: "성함", value: "이름" },
-    { label: "전화번호", value: "010-1234-4567" },
+    { label: "성함", value: contract.client_name || "" },
+    { label: "전화번호", value: formatPhone(contract.client_phone) },
   ];
 
   const contractee_data = [
     { label: "성함", value: "이름" },
     { label: "전화번호", value: "010-7894-4568" },
-    { label: "계좌번호", value: "우리은행 1234-456-789789" },
+    { label: "계좌번호", value: `${contract.bank} ${contract.account_number}` },
   ];
 
   return (
