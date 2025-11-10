4c8f302b939b04f4554ac0bfd53b2e1af5a067ee
SooY2 <ssy67366736@gmail.com>
2025-11-10 19:17:57 +0900

    feat: status-SETTLED view

--- src/components/trade/views/TradeViewSettled.jsx
+++ src/components/trade/views/TradeViewSettled.jsx
@@ -3,10 +3,15 @@ import TradeStepIndicator from "../TradeStepIndicator";
 import InfoText from "@/components/common/InfoText";
 
 const TradeViewSettled = () => {
+  const { tradeData } = useTradeDataStore();
+  const { clientName, freelancerName } = tradeData;
   return (
     <TradeViewLayout>
       <InfoText
-        mainTexts={["000(갑)님과 000(을)님의", "거래 페이지입니다."]}
+        mainTexts={[
+          `${clientName}님과 ${freelancerName}님의`,
+          "거래 페이지입니다.",
+        ]}
         subText="대금 지급이 완료되었어요! 거래가 성공적으로 마무리되었습니다."
       />
       <TradeStepIndicator currentStep={5} />
