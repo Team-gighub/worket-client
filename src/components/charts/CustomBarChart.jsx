45ed5f1e5de03958d7af57b38a42885bc4f8b6d0
yuunseo <dbstj7457@naver.com>
2025-11-11 16:37:49 +0900

    feat: 막대 그래프 컴포넌트 생성

---
+++ src/components/charts/CustomBarChart.jsx
@@ -0,0 +1,51 @@
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  ResponsiveContainer,
+  Cell,
+  LabelList,
+} from "recharts";
+import formatKRW from "@/app/utils/KRWFormatter";
+
+const CustomBarChart = ({ chartData = [], type = "INCOME" }) => {
+  const formattedData = chartData.map((item) => ({
+    month: `${Number(item.month.split("-")[1])}월`, // "2025-09" → "9월"
+    value: type === "INCOME" ? item.income : item.transactions,
+  }));
+
+  const colors = ["#E6E4FF", "#E6E4FF", "#5E4FE4"]; // point-purple-100("#E6E4FF"), primary("#5E4FE4")
+
+  return (
+    <div className="h-[35rem] select-none">
+      <ResponsiveContainer>
+        <BarChart
+          data={formattedData}
+          barCategoryGap="30%"
+          style={{ pointerEvents: "none" }} // 클릭 방지
+        >
+          <XAxis dataKey="month" axisLine={{ stroke: "transparent" }} />
+          <Bar
+            dataKey="value"
+            radius={[50, 50, 50, 50]}
+            isAnimationActive={true}
+          >
+            {/*LabelList로 포맷팅된 값 표시 */}
+            <LabelList
+              dataKey="value"
+              position="top"
+              fontSize={14}
+              fontWeight="bold"
+              formatter={(value) => formatKRW(value)}
+            />
+            {formattedData.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={colors[index]} />
+            ))}
+          </Bar>
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default CustomBarChart;
