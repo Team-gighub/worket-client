a4eaccca32d5e33d63faa4311d9ed2653bec1512
SooY2 <ssy67366736@gmail.com>
2025-11-11 09:32:06 +0900

    feat: store 초기세팅 컴포넌트

---
+++ src/components/trade/TradeDataHydrator.jsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { useTradeDataStore } from "@/stores/tradeDataStore";
+
+const TradeDataHydrator = ({ initialData, children }) => {
+  const { setTradeData } = useTradeDataStore();
+  const hasHydrated = useRef(false);
+
+  useEffect(() => {
+    if (!hasHydrated.current) {
+      setTradeData(stableInitialData);
+      hasHydrated.current = true;
+      console.log("Trade data hydrated:", stableInitialData);
+    }
+  }, [initialData]);
+
+  return children;
+};
+
+export default TradeDataHydrator;
