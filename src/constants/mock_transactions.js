353984de91dac6fb2ba2472c7ddb5e36e4ce8f3f
SooY2 <ssy67366736@gmail.com>
2025-11-13 17:43:48 +0900

    chore: 계약서 상세 더미데이터 생성

---
+++ src/constants/mock_transactions.js
@@ -0,0 +1,37 @@
+import { MOCK_CONTRACT } from "./mock_contracts";
+
+export const MOCK_TRANSACTION_DETAIL = {
+  status: "SIGNED",
+  signedAt: "2025-11-11T20:00:42.074Z",
+  depositHoldAt: "2025-11-11T16:08:55.215Z",
+  paymentConfirmedAt: "2025-11-11T16:49:24.553Z",
+  settledAt: "2025-11-12T02:40:58.476Z",
+  createdAt: "2025-11-12T07:06:05.705Z",
+  contractId: 956,
+  settledAmount: 715.89,
+  contractFileUrl: "https://loremflickr.com/167/2526?lock=3238486552340916",
+  ...MOCK_CONTRACT,
+};
+
+{
+  /**
+MOCK_CONTRACT
+
+  contractInfo: {
+    title: "프로젝트명",
+    amount: 150000,
+    startDate: "2025-12-05",
+    endDate: "2025-03-13",
+  },
+  clientInfo: {
+    name: "이영은",
+    phone: "010-2815-1244",
+  },
+  freelancerInfo: {
+    name: "김윤서",
+    phone: "010-3923-4054",
+    account: "135165135845",
+    bank: "국민은행",
+  },
+*/
+}
