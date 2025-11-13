16446d5a413cf7fb584ba295663b7b0e39074008
yuunseo <dbstj7457@naver.com>
2025-11-13 12:05:36 +0900

    refactor: Pin 공통 컴포넌트로 변경

--- src/app/pin/page.jsx
+++ src/app/pin/page.jsx
@@ -1,8 +1,22 @@
+"use client";
 import "@/app/globals.css";
+import PinInputForm from "@/components/common/PinInputForm";
+import { useRouter } from "next/navigation";
 
 /* 간편 비밀번호 등록 페이지 */
-const MyPage = () => {
-  return <div></div>;
+const PinPage = () => {
+  const router = useRouter();
+
+  return (
+    <div className="h-full">
+      <PinInputForm
+        mode="setup"
+        onSuccess={(pin) => {
+          router.push("/dashboard");
+        }}
+      />
+    </div>
+  );
 };
 
-export default MyPage;
+export default PinPage;
