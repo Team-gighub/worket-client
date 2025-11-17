5dd6512e30514fc82099dc29e80739166f47b153
SooY2 <ssy67366736@gmail.com>
2025-11-17 17:22:40 +0900

    fix: page.jsx -> page.js

--- src/app/trade/[id]/layout.js
+++ src/app/trade/[id]/layout.js
@@ -1,16 +1,16 @@
-import getTransactionById from "@/app/api/fetchTransactions";
 import TradeDataHydrator from "@/components/trade/TradeDataHydrator";
 import TradeViewLoginRequired from "@/components/trade/views/TradeViewLoginRequired";
+import { getTransactionsDetail } from "@/lib/api/server/transactionServices";
 import { getServerAuthTokens } from "@/lib/authUtils";
 
 const TradeLayout = async ({ params, children }) => {
-  const { id } = params;
+  const { id } = await params;
   const { accessToken } = await getServerAuthTokens();
   if (!accessToken) {
     return <TradeViewLoginRequired />;
   }
 
-  const data = await getTransactionById(id);
+  const { data } = await getTransactionsDetail(id);
   return <TradeDataHydrator initialData={data}>{children}</TradeDataHydrator>;
 };
 
