1adfce081cfe5ff4214e16114830645c714cba9b
noeyigg <142196324+noeyigg@users.noreply.github.com>
2025-11-17 14:55:20 +0900

    fix: 충돌 해결

--- src/app/transactions/layout.js
+++ src/app/transactions/layout.js
@@ -23,7 +23,7 @@ const TransactionLayout = ({ children }) => {
   } else if (pathname.startsWith("/transactions/create")) {
     headerTitle = "거래 등록";
     canGoBack = true;
-  } else if (pathname.startsWith("/transactions/upload") {
+  } else if (pathname.startsWith("/transactions/upload")) {
     headerTitle = "거래 업로드";
     canGoBack = true;
   } else if (/^\/transactions\/\d+$/.test(pathname)) {
