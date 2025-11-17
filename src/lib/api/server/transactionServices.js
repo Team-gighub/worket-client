814b83cd59b66951f629b70a9a9de702d090d384
SooY2 <ssy67366736@gmail.com>
2025-11-17 10:18:06 +0900

    feat: transactionServices (server)

---
+++ src/lib/api/server/transactionServices.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { createServerAxiosInstance } from "../instances/serverApiInstance";
+
+/** 거래 전체 조회 (GET /transactions) */
+const getTransactions = async () => {
+  const serverInstance = await createServerAxiosInstance();
+  return serverInstance.get(`/transactions`);
+};
+
+/** 거래 상세 조회 (GET /transactions/{transactionId}) */
+const getTransactionsDetail = async (transactionId) => {
+  const serverInstance = await createServerAxiosInstance();
+  return serverInstance.get(`/transactions/${transactionId}`);
+};
+
+/** 거래 정보 미리보기 = (GET /transactions/{transactionId}/preview) */
+const getTransactionsPreview = async (transactionId) => {
+  return axios.get(
+    process.env.NEXT_PUBLIC_API_BASE_URL +
+      `/transactions/${transactionId}/preview`,
+  );
+};
+
+/** 거래 접근권한 판단 (GET /transactions/{transactionId}/permissions) */
+const getTransactionsPermissions = async (transactionId) => {
+  const serverInstance = await createServerAxiosInstance();
+  return serverInstance.get(`/transactions/${transactionId}/permissions `);
+};
+
+export {
+  getTransactions,
+  getTransactionsDetail,
+  getTransactionsPreview,
+  getTransactionsPermissions,
+};
