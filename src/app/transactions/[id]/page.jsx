737aff1e28bdb00e57cc94cfb1b2efda90cf8e74
noeyigg <miyg2003@naver.com>
2025-11-19 16:45:09 +0900

    refactor: 백엔드 응답에 맞게 데이터 수정

--- src/app/transactions/[id]/page.jsx
+++ src/app/transactions/[id]/page.jsx
@@ -4,43 +4,31 @@ import MainButton from "@/components/common/MainButton";
 import TransactionStatusBox from "@/components/transactions/TransactionStatusBox";
 import TransactionTimeline from "@/components/transactions/TransactionTimeLine";
 import TransactionInfo from "@/components/transactions/TransactionInfo";
-import getTransactionById from "@/app/api/getTransactionById";
+import { getTransactionsDetail } from "@/lib/api/server/transactionServices";
 import LinkIcon from "@/assets/link.png";
 import ExternalLinkIcon from "@/assets/external-link.png";
-
-// 참고: getTransactionById가 반환하는 mock data 구조
-// const transaction = {
-//   client_name: "홍길동",
-//   freelancer_bank: "우리",
-//   freelancer_account: "1001-02-110-1254",
-//   contract_start_date: "2024-01-15 10:30:00",
-//   contract_end_date: "2024-01-15 10:30:00",
-//   contract_created_at: "2024-01-15 10:30:00",
-//   title: "워켓 프론트엔드 개발",
-//   price: "10,000",
-//   status: "SETTLED",
-//   created_at: "2024-01-15 10:30:00",
-//   signed_at: "2024-01-15 10:30:00",
-//   deposit_hold_at: "2024-01-15 10:30:00",
-//   payment_confirmed_at: "2024-01-15 10:30:00",
-//   settled_at: "2024-01-15 10:30:00",
-// };
+import formatKRW from "@/app/utils/KRWFormatter";
 
 /* 거래 페이지 */
 const Transaction = async ({ params }) => {
   const resolvedParams = await params;
   const id = resolvedParams.id;
-  const transaction = await getTransactionById(id);
+  const transaction = await getTransactionsDetail(id);
 
   return (
     <div>
-      <InfoText mainTexts={[transaction.title, `${transaction.price} 원`]} />
+      <InfoText
+        mainTexts={[
+          transaction.data.contractInfo.title,
+          `${formatKRW(transaction.data.contractInfo.amount)} 원`,
+        ]}
+      />
 
       {/* 거래 상태 표시 섹션 */}
-      <TransactionStatusBox status={transaction.status} />
+      <TransactionStatusBox status={transaction.data.status} />
 
       {/* 거래 타임라인 섹션 */}
-      <TransactionTimeline data={transaction} />
+      <TransactionTimeline data={transaction.data} />
 
       {/* 거래페이지 링크 복사 섹션  */}
       <div className="m-[2rem] flex flex-col">
@@ -60,7 +48,7 @@ const Transaction = async ({ params }) => {
 
       {/* 거래 정보 섹션 */}
       <div>
-        <TransactionInfo data={transaction} />
+        <TransactionInfo data={transaction.data} />
       </div>
 
       {/* 계약서 원본 보기 섹션  */}
