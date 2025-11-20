9fbd9d2018eb879aeeef74fc627331ad1b7c2777
youm0524 <juliet0524@naver.com>
2025-11-20 16:44:13 +0900

    feat: 서명 DB 저장 로직

--- src/components/common/Signature.jsx
+++ src/components/common/Signature.jsx
@@ -1,28 +1,27 @@
 "use client";
-
 import SignatureCanvas from "react-signature-canvas";
 import useSignature from "../../hooks/useSignature";
 import DualButtons from "./DualButtons";
 import MainButton from "./MainButton";
-
+import { postContractsSignatures } from "@/lib/api/client/contractServices";
 /**
  * Signature Component
  *
  * @param {function(): void} onClose - 서명 완료 동작이 수행되고 나면, 바텀시트가 닫히도록 지정하는 함수
  * @param {function(): Promise<string>} getPresignedUrl - 서버에서 S3 업로드용 presigned URL을 받아오는 비동기 함수
  */
-const Signature = ({ onClose, getPresignedUrl }) => {
-  const { signatureRef, clearSignature, saveSignature } = useSignature(
-    onClose,
-    getPresignedUrl,
-  );
-
+const Signature = ({ onClose }) => {
+  const { signatureRef, clearSignature, saveSignature } = useSignature(onClose);
+  const saveSignatureAndPost = async () => {
+    const signUrl = await saveSignature();
+    const contractId = sessionStorage.getItem("contractId");
+    postContractsSignatures(contractId, signUrl);
+  };
   return (
     <div className="relative rounded-lg p-4 mt-3 ">
       <span className="absolute top-10 left-11 pretendard-semibold-16 text-basic-400">
         서명란
       </span>
-
       {/* Signature Canvas 영역 */}
       <SignatureCanvas
         ref={signatureRef}
@@ -30,7 +29,6 @@ const Signature = ({ onClose, getPresignedUrl }) => {
           className: "bg-basic-200 rounded-lg w-full h-[150px]",
         }}
       />
-
       {/* 버튼 영역 */}
       <div className="w-full flex gap-[1rem] justify-between my-9">
         <MainButton
@@ -41,12 +39,11 @@ const Signature = ({ onClose, getPresignedUrl }) => {
         ></MainButton>
         <MainButton
           text="서명완료"
-          onClick={saveSignature}
+          onClick={saveSignatureAndPost}
           isFullWidth={true}
         ></MainButton>
       </div>
     </div>
   );
 };
-
 export default Signature;
