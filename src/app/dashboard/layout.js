691419949c7c47e2b1b601895b8800ce8478a025
youm0524 <juliet0524@naver.com>
2025-11-21 14:26:47 +0900

    fix: dashboard delete

--- src/app/dashboard/layout.js
+++
@@ -1,8 +0,0 @@
-import Header from "@/components/common/Header";
-import PageLayout from "@/components/layouts/PageLayout";
-
-const Layout = ({ children }) => {
-  return <PageLayout header={<Header />}>{children}</PageLayout>;
-};
-
-export default Layout;
