a84647cd254bd1d54ca44b1d26452b9e6cd37f0d
SooY2 <ssy67366736@gmail.com>
2025-11-24 12:28:03 +0900

    feat: previewUrl store 추가

--- src/stores/signatureStore.js
+++ src/stores/signatureStore.js
@@ -5,10 +5,13 @@ export const useSignatureStore = create((set) => ({
   signUrl: null,
   isUploading: false,
   error: null,
+  tempPreviewUrl: null,
 
   // Simple actions (상태 변경만)
   setSignUrl: (url) => set({ signUrl: url, error: null }),
   setIsUploading: (loading) => set({ isUploading: loading }),
   setError: (error) => set({ error }),
   clearSignature: () => set({ signUrl: null, error: null }),
+  setTempPreviewUrl: (url) => set({ tempPreviewUrl: url, error: null }),
+  clearTempPreviewUrl: () => set({ tempPreviewUrl: null, error: null }),
 }));
