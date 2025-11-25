8f5151bdbe22ea8a53320c6b9dc5333494916a2a
SooY2 <ssy67366736@gmail.com>
2025-11-25 14:14:49 +0900

    feat: role에 따른 분기 가능하도록

--- src/lib/api/instances/clientApiInstance.js
+++ src/lib/api/instances/clientApiInstance.js
@@ -1,7 +1,7 @@
 "use client";
 import axios from "axios";
 
-export const createClientAxiosInstance = () => {
+export const createClientAxiosInstance = (transactionId = "") => {
   const instance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
     headers: {
@@ -20,21 +20,29 @@ export const createClientAxiosInstance = () => {
           case "ACCESS_1001":
             return err.response;
           default:
-            window.location.href = "/login";
+            if (transactionId) {
+              return err.response;
+            } else {
+              window.location.href = "/login";
+            }
         }
       }
 
       if (err.response?.status === 403) {
-        console.log(err.response.data.customCode === "AUTH_2001");
         switch (err.response.data.customCode) {
           case "ACCESS_3001":
             return err.response;
           case "AUTH_2001":
             window.location.href = "/signup";
             return;
-          case "AUTH_3001":
+          case "AUTH_3011":
             window.location.href = "/passcode";
             return;
+          case "AUTH_3021":
+            if (transactionId) {
+              window.location.href = `/trade/passcode/${transactionId}`;
+              return;
+            }
           default:
             return Promise.reject(err.response?.data || new Error(err.message));
         }
