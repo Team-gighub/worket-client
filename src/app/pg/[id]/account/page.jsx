b89755062b53663a5a061eac7700afc46067dfe0
youm0524 <juliet0524@naver.com>
2025-11-25 11:31:07 +0900

    feat: 패스코드 달기

--- src/app/pg/[id]/account/page.jsx
+++ src/app/pg/[id]/account/page.jsx
@@ -2,23 +2,30 @@
 import InfoText from "@/components/common/InfoText";
 import { usePaymentStore } from "@/stores/paymentStore";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useRouter, useParams } from "next/navigation";
 import InputAccount from "@/components/common/InputField";
 import DualButtons from "@/components/common/DualButtons";
 import { useState, useEffect } from "react";
+import TradePasscodeForm from "@/components/passcode/TradePasscodeForm";
 
-const SelectAccount = ({ id }) => {
+const SelectAccount = () => {
+  //console.log(passcode);
   const router = useRouter();
+  const { id } = useParams();
   const [accountNumber, setAccountNumber] = useState("");
+  // passcodeForm출력
+  const [showPassCodeForm, setShowPassCodeForm] = useState(false);
+  // passcode 받기
+  const [passcode, setPasscode] = useState("");
   //은행 정보(은행, 은행 아이콘) 가져오기
   const selectedBankInfo = usePaymentStore((state) => state.selectedBankInfo);
   const selectAccount = usePaymentStore((state) => state.selectAccount);
-  // redirect 처리
+
   useEffect(() => {
     if (!selectedBankInfo || !selectedBankInfo.id) {
-      router.push("/pg");
+      router.push(`/pg/${id}`);
     }
-  }, [selectedBankInfo, router]);
+  }, [selectedBankInfo, router, id]);
 
   if (!selectedBankInfo || !selectedBankInfo.id) return null;
 
@@ -31,22 +38,34 @@ const SelectAccount = ({ id }) => {
       alert("계좌번호를 입력해주세요.");
       return;
     }
-    //계좌정보 넣기
+    // 계좌 정보 store 저장
     selectAccount({ accountNumber });
-    router.push("/pg/confirm");
+    setShowPassCodeForm(true);
   };
 
   const handleSubBtn = () => {
-    router.push("/pg");
+    router.push(`/pg/${id}`);
   };
-
+  const handlePasscodeComplete = (enteredPasscode) => {
+    setPasscode(enteredPasscode); // SelectAccount state에 저장
+    console.log("입력 완료 패스코드:", enteredPasscode);
+    //TODO: PG server API 호출-> accountNumber,selectedBankId 같이 넘김
+  };
   const { label, icon } = selectedBankInfo;
 
   return (
     <div className="h-full flex flex-col bg-basic-100 py-[4rem]">
-      {/* 회색 카드 */}
-      <div className="w-full max-w-6xl h-[90%] bg-basic-200 rounded-2xl shadow-lg p-8 overflow-hidden flex flex-col">
-        {/* 회색 카드 상단 헤더 라인 */}
+      {/* 회색 카드 래퍼 */}
+      <div className="w-full max-w-6xl h-[90%] bg-basic-200 rounded-2xl shadow-lg p-8 overflow-hidden flex flex-col relative">
+        {showPassCodeForm && (
+          <div className="absolute inset-0 bg-basic-200 z-20 flex justify-center items-center">
+            <TradePasscodeForm
+              handlePasscodeComplete={handlePasscodeComplete}
+            />
+          </div>
+        )}
+
+        {/* 회색 카드 상단 헤더 라인 (PasscodeForm이 덮을 영역) */}
         <div className="border-t border-b border-gray-300 py-6 mb-12">
           <div className="flex flex-row items-center justify-between p-4">
             {/* 왼쪽 텍스트 */}
@@ -78,39 +97,44 @@ const SelectAccount = ({ id }) => {
           </div>
         </div>
 
-        {/* 카드 내부 콘텐츠 */}
+        {/* 카드 내부 콘텐츠 (showPassCodeForm이 false일 때만 표시) */}
         <div className="flex flex-col justify-between h-full pb-[3rem]">
-          <div>
-            <div className="flex gap-4 items-center w-[31rem] mb-8 mt-8 pl-8">
-              {icon && (
-                <div className="relative w-10 h-10 flex-shrink-0">
-                  <Image
-                    src={icon.src}
-                    alt={icon.alt}
-                    fill
-                    sizes="40px"
-                    className="object-contain"
-                  />
+          {!showPassCodeForm && (
+            <>
+              <div>
+                <div className="flex gap-4 items-center w-[31rem] mb-8 mt-8 pl-8">
+                  {icon && (
+                    <div className="relative w-10 h-10 flex-shrink-0">
+                      <Image
+                        src={icon.src}
+                        alt={icon.alt}
+                        fill
+                        sizes="40px"
+                        className="object-contain"
+                      />
+                    </div>
+                  )}
+                  <div className="text-lg font-medium">{label}</div>
                 </div>
-              )}
-              <div className="text-lg font-medium">{label}</div>
-            </div>
-            <InputAccount
-              onChange={handleAccountChange}
-              question="계좌번호"
-              placeholder="계좌번호를 입력하세요"
-              type="number"
-            />
-          </div>
 
-          <DualButtons
-            mainText="확인"
-            onMainClick={handleMainBtn}
-            subText="이전"
-            onSubClick={handleSubBtn}
-            width="34rem"
-            mainButtonTheme="secondary"
-          />
+                <InputAccount
+                  onChange={handleAccountChange}
+                  question="계좌번호"
+                  placeholder="계좌번호를 입력하세요"
+                  type="number"
+                />
+              </div>
+
+              <DualButtons
+                mainText="확인"
+                onMainClick={handleMainBtn} // '확인' 버튼 클릭 시 showPassCodeForm = true
+                subText="이전"
+                onSubClick={handleSubBtn}
+                width="34rem"
+                mainButtonTheme="secondary"
+              />
+            </>
+          )}
         </div>
       </div>
     </div>
