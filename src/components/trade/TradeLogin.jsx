792c96d34ff1a99584b9be77ef4f75a87de38e7b
SooY2 <ssy67366736@gmail.com>
2025-11-25 14:50:28 +0900

    chore: 주석 제거

--- src/components/trade/TradeLogin.jsx
+++ src/components/trade/TradeLogin.jsx
@@ -2,7 +2,6 @@
 import MainButton from "../common/MainButton";
 
 const TradeLogin = ({ id }) => {
-  console.log(id);
   const handleKakaoLogin = () => {
     const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
     window.location.href = `${baseUrl}/oauth2/authorization/kakao?state=CLIENT:tx=${id}`;
