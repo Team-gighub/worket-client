a801e5d4936a098b301d7afaa84ead4c26e23f58
SooY2 <ssy67366736@gmail.com>
2025-11-25 14:23:52 +0900

    feat: 커스텀에러에 따른 렌더링

--- src/app/trade/[id]/layout.js
+++ src/app/trade/[id]/layout.js
@@ -9,24 +9,35 @@ import { getServerAuthTokens } from "@/lib/authUtils";
 
 const TradeLayout = async ({ params, children }) => {
   const { id } = await params;
-  const { accessToken } = await getServerAuthTokens();
-  if (!accessToken) {
+  try {
+    //(case1) Cookie에 accessToken이 없을 경우
+    const { accessToken } = await getServerAuthTokens();
+    if (!accessToken) {
+      return <TradeViewLoginRequired id={id} />;
+    }
+
+    //(case2) 권한 여부 확인
+    const { data: permissionData } = await getTransactionsPermissions(id);
+    if (permissionData.customCode === "ACCESS_1001") {
+      return <TradeViewLoginRequired id={id} />;
+    } else if (
+      permissionData.userRole === "FREELANCER" ||
+      permissionData.customCode === "ACCESS_3001"
+    )
+      return <TradeViewNoPermission />;
+
+    //(case3) 거래 정보 가져오기
+    const { data: tradeData } = await getTransactionsDetail(id, "CLIENT");
+    if (tradeData?.httpStatus === 401) {
+      return <TradeViewLoginRequired id={id} />;
+    }
+
+    return (
+      <TradeDataHydrator initialData={tradeData}>{children}</TradeDataHydrator>
+    );
+  } catch (error) {
     return <TradeViewLoginRequired id={id} />;
   }
-
-  const { data: permissionData } = await getTransactionsPermissions(id);
-  if (permissionData.customCode === "ACCESS_1001") {
-    return <TradeViewLoginRequired id={id} />;
-  } else if (
-    permissionData.userRole === "FREELANCER" ||
-    permissionData.customCode === "ACCESS_3001"
-  )
-    return <TradeViewNoPermission />;
-
-  const { data: tradeData } = await getTransactionsDetail(id);
-  return (
-    <TradeDataHydrator initialData={tradeData}>{children}</TradeDataHydrator>
-  );
 };
 
 export default TradeLayout;
