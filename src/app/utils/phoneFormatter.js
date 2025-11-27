581bb057c5c144dcf28f8dcb43f533c78f141a4e
SooY2 <ssy67366736@gmail.com>
2025-11-27 11:06:15 +0900

    refac: phoneFomatter 수정

--- src/app/utils/phoneFormatter.js
+++ src/app/utils/phoneFormatter.js
@@ -1,25 +1,21 @@
 /**
  *
- * @param {string} phone 010-xxxx-xxxx 형식의 전화번호 포매터
+ * @param {string} value 010-xxxx-xxxx 형식의 전화번호 포매터
  * @returns
  */
 export const formatPhone = (value) => {
   if (!value) return "";
 
-  // 숫자만 추출
-  let cleaned = value.replace(/\D/g, "");
+  const cleaned = value.replace(/\D/g, "").slice(0, 11);
+  const { length } = cleaned;
 
-  // 11자리 이상 입력 안되게 제한
-  cleaned = cleaned.slice(0, 11);
-
-  // 자리수에 따라 동적으로 포맷팅
-  if (cleaned.length < 4) {
-    return cleaned; // 3자리 이하 → 그냥 표시
+  if (length < 4) {
+    return cleaned;
   }
-
-  if (cleaned.length < 7) {
+  if (length < 8) {
     return `${cleaned.slice(0, 3)}-${cleaned.slice(3)}`;
   }
 
+  // 11자리 번호 (e.g., 010-xxxx-xxxx)
   return `${cleaned.slice(0, 3)}-${cleaned.slice(3, 7)}-${cleaned.slice(7)}`;
 };
