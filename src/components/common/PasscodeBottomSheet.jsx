b7ff774fe6799a02a48bb290202dccc045383c55
SooY2 <ssy67366736@gmail.com>
2025-11-28 10:22:51 +0900

    chore: 안쓰는 import 제거

--- src/components/common/PasscodeBottomSheet.jsx
+++ src/components/common/PasscodeBottomSheet.jsx
@@ -1,11 +1,9 @@
 "use client";
 import React from "react";
 import BottomSheet from "./BottomSheet";
-import { useRouter } from "next/navigation";
 import PasscodeForm from "../passcode/PasscodeForm";
 
 const PasscodeBottomSheet = ({ isOpen, onClose, handlePasscodeComplete }) => {
-  const router = useRouter();
   return (
     <BottomSheet isOpen={isOpen} onClose={onClose}>
       <PasscodeForm
