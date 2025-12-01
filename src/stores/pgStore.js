092cd84c75e56ed4fe8286f415ae6421db805b74
SooY2 <ssy67366736@gmail.com>
2025-12-01 12:10:42 +0900

    feat: pg 뷰에 넘겨주어야하는 정보들 store에 저장

---
+++ src/stores/pgStore.js
@@ -0,0 +1,88 @@
+// pg 뷰에 보내져야할 데이터들 저장하는 store
+import { create } from "zustand";
+
+export const usePgStore = create((set) => ({
+  payload: {
+    merchantId: "",
+    userId: "",
+    userName: "",
+    productName: "",
+    amount: 0,
+    orderNo: "",
+    payerInfo: {
+      accountNo: "",
+      bankCode: "",
+      name: "",
+      phone: "",
+    },
+    payeeInfo: {
+      accountNo: "",
+      bankCode: "",
+      name: "",
+      phone: "",
+    },
+    successUrl: "",
+    failUrl: "",
+  },
+  setPgStore: (payload) =>
+    set((state) => ({
+      ...state,
+      payload: payload,
+    })),
+
+  setField: (key, value) =>
+    set((state) => ({
+      payload: {
+        ...state.payload,
+        [key]: value,
+      },
+    })),
+
+  setPayerInfo: (data) =>
+    set((state) => ({
+      payload: {
+        ...state.payload,
+        payerInfo: {
+          ...state.payload.payerInfo,
+          ...data,
+        },
+      },
+    })),
+
+  setPayeeInfo: (data) =>
+    set((state) => ({
+      payload: {
+        ...state.payload,
+        payeeInfo: {
+          ...state.payload.payeeInfo,
+          ...data,
+        },
+      },
+    })),
+
+  reset: () =>
+    set(() => ({
+      payload: {
+        merchantId: "",
+        userId: "",
+        userName: "",
+        productName: "",
+        amount: 0,
+        orderNo: "",
+        payerInfo: {
+          accountNo: "",
+          bankCode: "",
+          name: "",
+          phone: "",
+        },
+        payeeInfo: {
+          accountNo: "",
+          bankCode: "",
+          name: "",
+          phone: "",
+        },
+        successUrl: "",
+        failUrl: "",
+      },
+    })),
+}));
