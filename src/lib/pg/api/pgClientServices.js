ccffc21901cd7846e96fb36e1359c8ace38620f8
SooY2 <ssy67366736@gmail.com>
2025-12-02 09:36:15 +0900

    fix: 서버를 통해 통신하는 APi 분리

--- src/lib/pg/api/pgClientServices.js
+++ src/lib/pg/api/pgClientServices.js
@@ -1,12 +1,7 @@
 import { createPgApiInstance } from "./pgApiInstance";
 
+//pg 서버로 바로 요청하는 api (결제 인증)
 export const postPgPaymentAuthorize = (payload) => {
   const clientInstance = createPgApiInstance();
   return clientInstance.post(`/payment/authorize`, payload);
-};
-
-export const postPgPaymentApproval = (payload) => {
-  console.log(payload);
-  const clientInstance = createPgApiInstance();
-  return clientInstance.post(`/payment/approval`, payload);
 };
