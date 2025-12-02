b87127b900ec530fcabe00b7008567015e9c8794
SooY2 <ssy67366736@gmail.com>
2025-12-02 10:07:00 +0900

    fix: pg baseurl 수정

--- src/lib/pg/api/pgClientServices.js
+++ src/lib/pg/api/pgClientServices.js
@@ -3,5 +3,5 @@ import { createPgApiInstance } from "./pgApiInstance";
 //pg 서버로 바로 요청하는 api (결제 인증)
 export const postPgPaymentAuthorize = (payload) => {
   const clientInstance = createPgApiInstance();
-  return clientInstance.post(`/payment/authorize`, payload);
+  return clientInstance.post(`api/v1/payment/authorize`, payload);
 };
