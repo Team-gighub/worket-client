55e35eea6b1d318cdc6f0727f3e32866c5ac03c6
yuunseo <dbstj7457@naver.com>
2025-12-04 11:17:11 +0900

    refactor: api로 수정

--- src/lib/api/server/adminContractService.js
+++ src/lib/api/server/adminContractService.js
@@ -3,13 +3,13 @@ const serverInstance = createServerAxiosInstance();
 
 // 특정 계약 수정 요청 상세 조회
 const getContractDetail = (modificationId) => {
-  // return serverInstance.get(`/contract-modifications/${modificationId}`);
-  return (
-    mockRevisionDetails[modificationId] || {
-      status: "error",
-      data: null,
-    }
-  );
+  return serverInstance.get(`/contract-modifications/${modificationId}`);
+  // return (
+  //   mockRevisionDetails[modificationId] || {
+  //     status: "error",
+  //     data: null,
+  //   }
+  // );
 };
 
 // 계약 수정 승인
